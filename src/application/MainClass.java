4c6915e0fc86f870353025e0b7e16e5e00370cad
Polianna Basso <poliannabassolg@gmail.com>
2022-07-15 15:54:57 -0300

    Class Position created

--- src/application/MainClass.java
+++ src/application/MainClass.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class MainClass {
 
     public static void main(String[] args) {
-        System.out.println("Imprimindo");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
     
 }
